fc98dad229ce4dce16c36294760a89c82c6a010b
romulo-soares <bromulosoares@gmail.com>
2019-07-14 16:47:10 -0300

    instance execution script

---
+++ execute-instances.sh
@@ -0,0 +1,3 @@
+mvn clean compile assembly:single
+java -cp target/sdatvconcorrenciaparte1-1.0-SNAPSHOT-jar-with-dependencies.jar br.edu.ifpb.sdatvconcorrenciaparte1.loader.Loader & 
+java -cp target/sdatvconcorrenciaparte1-1.0-SNAPSHOT-jar-with-dependencies.jar br.edu.ifpb.sdatvconcorrenciaparte1.loader.Loader
